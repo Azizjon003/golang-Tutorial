b5e3e1ef097b388f1ab853da5e276322758c3a1b
Azizjon <94430020+Azizjon003@users.noreply.github.com>
2022-12-27 22:22:18 +0500

    conversion type go

--- 04.conversion/main.go
+++ 04.conversion/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func main(){
-	
+	fmt.Println("Welcome to our pizza App")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter your age: ")
+	age, _ := reader.ReadString('\n')
+
+  fmt.Println("This is Age ", age)
+
+	ageFloat,err := strconv.ParseFloat(strings.TrimSpace(age), 64)
+
+
+	if(err != nil){
+		fmt.Println("Error", err)
+	}else{
+		fmt.Println("This is Age ", ageFloat+1)
+	}
 }
