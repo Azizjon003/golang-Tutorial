01adaff27d7f2c1821f26b80cc56c9b555712bdd
Azizjon <94430020+Azizjon003@users.noreply.github.com>
2022-12-30 18:00:58 +0500

    types boolean.go file added

---
+++ types/boolean.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+const  constVar string  = "Hello World"
+func main() {
+
+
+var isBool bool =  true;
+fmt.Println(isBool)
+fmt.Printf("this variable is of type %T \n", isBool)  // results true
+
+}
