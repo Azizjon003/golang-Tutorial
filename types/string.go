ca3b7197e0148fd3b59c8e545539301571522c7d
Azizjon <94430020+Azizjon003@users.noreply.github.com>
2022-12-30 18:12:37 +0500

    types string.go added

---
+++ types/string.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+const  constVar string  = "Hello World"
+func main() {
+var username string = "Aliqulov"
+fmt.Println("My username is ", username)
+fmt.Printf("this variable is of type %T \n", username)
+}
