09e9411c667833c4301de7dd227d711b105b9602
Azizjon <94430020+Azizjon003@users.noreply.github.com>
2023-01-01 15:06:49 +0500

    09.slices remove slices

--- 09.myslices/main.go
+++ 09.myslices/main.go
@@ -8,7 +8,9 @@ import (
 	
 fruitList  := []string{"apple", "banana", "orange"}
 	fmt.Printf("This is fruitList %T \n",fruitList)
+
 	fruitList = append(fruitList,"grape","mango","pineapple")
+fruitList = fruitList[4:]
 	fmt.Println("This is fruitList",fruitList)
 
 
@@ -23,6 +25,7 @@ fruitList  := []string{"apple", "banana", "orange"}
 	
 	hightScores = append(hightScores,900,800,50)
 	
+	// hightScores = hightScores[3:]
 	fmt.Println("This is hightScores",hightScores)
 	fmt.Printf("This is hightScores %T \n",hightScores)
 	sort.Ints(hightScores)
@@ -30,4 +33,22 @@ fruitList  := []string{"apple", "banana", "orange"}
 	fmt.Println("\nThis is hightScores is sorted",hightScores)
 	fmt.Println("This is hightScores length :(",len(hightScores),")")
 	fmt.Println("This is hightScores Is Sorted :(",sort.IntsAreSorted(hightScores),")")
- }
+
+	// remove slice element
+	var lang = []string{"go","java","python","c++","c#","ruby","php"}
+	index := 3
+	lang = append(lang[:index],lang[index+1:]...)
+	fmt.Println("This is lang",lang)
+
+
+
+	var langs = make([]string,3)
+	langs[0] = "go"
+	langs[1] = "java"
+	langs[2] = "python"
+	fmt.Println("This is langs",langs)
+	indx := 1
+	langs = append(langs[:indx],langs[indx+1:]...)
+
+fmt.Println("This is langs",langs)
+}
