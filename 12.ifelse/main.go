e7457e1055b65bed02400f812d31eaf2c299a43a
Azizjon <94430020+Azizjon003@users.noreply.github.com>
2023-01-03 13:52:40 +0500

    ifelse added

---
+++ 12.ifelse/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+ func main()  {
+	fmt.Println("This is if else statement")
+	logicCount  := 10
+	if logicCount >10{
+		fmt.Println("This is logicCount > 10")
+	}else if logicCount == 10{
+	fmt.Println("This is logicCount == 10")
+	}
+ }
